a7e68364f314b327d5fbb295c14f87263976a394
orbitadajogatina <orbitadajogatina@gmail.com>
2025-01-23 01:15:57 -0300

    fix: make those image emojis square

--- src/lib/components/Emoji.svelte
+++ src/lib/components/Emoji.svelte
@@ -3,7 +3,9 @@
 </script>
 
 {#if emoji.startsWith('$')}
-  <img src={emoji.match(/\$\[Imagem\]\((.*?)\)\$/)[1]} class="h-[2.2rem]" alt="Emoji com imagem">
+  <div class="aspect-square h-[2.2rem]">
+    <img src={emoji.match(/\$\[Imagem\]\((.*?)\)\$/)[1]} class="h-[2.2rem] mx-auto" alt="Emoji com imagem">
+  </div>
 {:else}
   <em-emoji native={emoji} set="twitter" size="2.2rem"></em-emoji>
 {/if}
