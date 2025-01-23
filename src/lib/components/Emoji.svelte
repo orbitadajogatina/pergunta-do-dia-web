3b86230e56788e3cbddbe6f117ee3a288dde944d
orbitadajogatina <orbitadajogatina@gmail.com>
2025-01-23 01:07:46 -0300

    feat: handle image emoji

--- src/lib/components/Emoji.svelte
+++ src/lib/components/Emoji.svelte
@@ -3,7 +3,7 @@
 </script>
 
 {#if emoji.startsWith('$')}
-   <!-- content here -->
+  <img src={emoji.match(/\$\[Imagem\]\((.*?)\)\$/)[1]} class="h-[2.2rem]" alt="Emoji com imagem">
 {:else}
   <em-emoji native={emoji} set="twitter" size="2.2rem"></em-emoji>
 {/if}
